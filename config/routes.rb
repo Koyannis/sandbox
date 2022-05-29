37e8979abe9bc59505040c43706f6ca5bb76e9ec
Remy Dufay <remy.dufay@gmail.com>
2022-05-29 09:59:13 +0200

    Bouton reset + ajustement du responsive

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/win", to: 'win#win'
+  get "/reset", to: "pages#reset"
 
 end
+
+
+
